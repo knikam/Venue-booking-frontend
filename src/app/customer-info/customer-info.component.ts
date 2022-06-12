3452b5fa1f1ccb2e2b0a8b79f03ea3a50e0c5364
kalpesh nikam <kalpeshnikam1080@gmail.com>
2022-06-12 10:36:38 +0530

    update

--- src/app/customer-info/customer-info.component.ts
+++ src/app/customer-info/customer-info.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerInfoComponent implements OnInit {
 
+  user:any
+  role:boolean = true;
   constructor() { }
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user') || '{}')
+    console.log(this.user)
+    if(localStorage.getItem('role') == "Dealer"){
+      this.role = false
+    }
   }
 
 }
